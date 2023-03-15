68809adef22e9b1622e531fe15b8ee54c9a48173
tejrajs <helloteju96@gmail.com>
2023-03-16 01:48:46 +1100

    initalized post entity with columns

--- api/src/post/entities/post.entity.ts
+++ api/src/post/entities/post.entity.ts
@@ -1,1 +1,28 @@
-export class Post {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('posts')
+export class Post {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    slug: string;
+
+    @Column()
+    content: string;
+
+    /**
+     @Column({
+        type: 'datetime',
+        default: () => 'NOW()',
+    })
+     */
+    @CreateDateColumn()
+    created_at: Date; // Creation date
+    
+    @UpdateDateColumn()
+    updated_at: Date; // Last updated date
+}
--- api/src/post/post.module.ts
+++ api/src/post/post.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { Post } from './entities/post.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [PostController],
-  providers: [PostService]
+  providers: [PostService],
+  imports:[
+    TypeOrmModule.forFeature([Post])
+  ]
 })
 export class PostModule {}
