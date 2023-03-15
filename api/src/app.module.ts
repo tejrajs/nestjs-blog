2495da39547a27ca740bc67cfc22c7b8ce7f55bb
tejrajs <helloteju96@gmail.com>
2023-03-16 01:46:52 +1100

    connected database with config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { DatabaseModule } from './config/database';
 import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [PostModule],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({}),
+    DatabaseModule,
+    PostModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
